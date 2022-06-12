f5af2fcb2d209713a5d2b34d5817543a67df3dfa
Jacob Bochmann <72769017+wascup@users.noreply.github.com>
2022-06-12 03:58:06 -0500

    Started working on Music List

--- src/SounderPanel.java
+++ src/SounderPanel.java
@@ -7,23 +7,42 @@ import java.util.ArrayList;
 
 
 public class SounderPanel extends JPanel {
-    ArrayList<Song> songs = new ArrayList<Song>();
+    ArrayList<Song> Songs = new ArrayList<Song>();
+    DefaultListModel SongModel = new DefaultListModel();
     ImageIcon songAlbum;
     public int albumSize = 200;
+    JList<Song> songList = new JList<Song>();
 
     public SounderPanel() {
-        setupGUI();
+        setLayout(null);
+        Thread guiThread = new Thread(this::setupGUI);
+        guiThread.start();
     }
     void setupGUI() {
-        setLayout(null);
         songAlbum = new ImageIcon();
         setBackground(Color.decode("#494949"));
         songAlbum = new ImageIcon("placeholders/album.png");
         JLabel currentSongAlbum = new JLabel(songAlbum);
         currentSongAlbum.setBounds(10, 10, albumSize, albumSize);
         add(currentSongAlbum);
+        songList.setBounds(albumSize + 20, 10, 275, 300);
+        songList.setBackground(Color.decode("#606060"));
+        songList.setForeground(Color.WHITE);
+        Thread addSong = new Thread(() -> {
+           Song newSong = addSong();
+           addSongToList(newSong);
+        });
+        addSong.start();
+        add(songList);
+        revalidate();
+        repaint();
     }
-
+    void addSongToList(Song newSong) {
+        Songs.add(newSong);
+        SongModel.add(SongModel.getSize(), newSong.getTitle());
+        songList.setModel(SongModel);
+        songList.add(Songs.get(Songs.size() - 1).getComponent());
+    }
 
     Song addSong() {
         Song newSong = new Song();
