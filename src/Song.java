1aa18a24f1f1e2bb6007e2730d803d28fef9bf56
Jacob Bochmann <72769017+wascup@users.noreply.github.com>
2022-06-13 00:55:00 -0500

    Rewrote the music selector and UI things
    rewrote the entire makeSong function
    
    added support to select multiple songs and fill in the gaps with dummy data to make it consistent.
    
    added track and year to UI and made the song list scrollable
    
    added some comments to understand better

--- src/Song.java
+++ src/Song.java
@@ -2,76 +2,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.mpatric.mp3agic.*;
 
 public class Song {
     private File file;
-    private byte[] albumart;
+    private Image albumart;
     private String title;
     private String artist;
     private String album;
     private String year;
     private String genre;
     private String track;
-    private String comment;
-    private String lyrics;
     private String filePath;
     private double length;
-    public Song makeSong() {
+    public Song[] makeSong() {
+        ArrayList<Song> songs = new ArrayList<>();
         try {
-            JFileChooser chooser = new JFileChooser();
-            chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            chooser.setAcceptAllFileFilterUsed(false);
-            chooser.setMultiSelectionEnabled(false);
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                this.file = chooser.getSelectedFile();
-                this.filePath = file.getAbsolutePath();
-                Mp3File mp3file = new Mp3File(filePath);
-                ID3v2 id3v2Tag = null;
+            FileDialog chooser = new FileDialog(new JFrame(), "Select a song", FileDialog.LOAD);
+            chooser.setMultipleMode(true);
+            chooser.setVisible(true);
+
+            for (File file : chooser.getFiles()) {
+                Song newSong = new Song();
+                newSong.file = file;
+                newSong.filePath = file.getAbsolutePath();
+                newSong.title = file.getName();
+                newSong.artist = "Unknown";
+                newSong.album = "Unknown";
+                newSong.year = "Unknown";
+                newSong.genre = "Unknown";
+                newSong.track = "Unknown";
+                newSong.length = 0;
+                newSong.albumart = new ImageIcon("placeholders/noart.png").getImage();
+
+                Mp3File mp3file = new Mp3File(file);
                 if (mp3file.hasId3v2Tag()) {
-                    id3v2Tag = mp3file.getId3v2Tag();
-                    this.title = id3v2Tag.getTitle();
-                    this.artist = id3v2Tag.getArtist();
-                    this.album = id3v2Tag.getAlbum();
-                    this.year = id3v2Tag.getYear();
-                    this.genre = id3v2Tag.getGenreDescription();
-                    this.track = id3v2Tag.getTrack();
-                    this.comment = id3v2Tag.getComment();
-                    this.lyrics = id3v2Tag.getLyrics();
+                    ID3v1 tag = mp3file.getId3v2Tag();
+                    newSong.file = file;
+                    newSong.title = tag.getTitle();
+                    newSong.artist = tag.getArtist();
+                    newSong.album = tag.getAlbum();
+                    newSong.year = tag.getYear();
+                    newSong.genre = String.valueOf(tag.getGenre());
+                    newSong.track = tag.getTrack();
+                    byte[] AlbumArt = mp3file.getId3v2Tag().getAlbumImage();
+                    if(AlbumArt != null) {
+                        newSong.albumart = new ImageIcon(AlbumArt).getImage();
+                    }
+                    newSong.filePath = file.getAbsolutePath();
+                    newSong.length = mp3file.getLengthInMilliseconds();
                 }
-                this.length = mp3file.getLengthInSeconds();
-                this.albumart = id3v2Tag.getAlbumImage();
+                songs.add(newSong);
             }
-        } catch (IOException | UnsupportedTagException | InvalidDataException e) {
-            e.printStackTrace();
+            } catch (InvalidDataException | IOException | UnsupportedTagException ex) {
+            throw new RuntimeException(ex);
         }
-        return this;
-    }
-    public Component getComponent() {
-        return new JLabel(this.getTitle());
+
+        return songs.toArray(new Song[songs.size()]);
     }
     public String getTitle() {
         return title;
     }
-    public void setTitle(String title) {
-        this.title = title;
-    }
     public String getArtist() {
         return artist;
     }
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
     public String getAlbum() {
         return album;
-    }
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-    public void setAlbumart(byte[] albumart) {
-        this.albumart = albumart;
     }
     public ImageIcon getAlbumart() {
         return new ImageIcon(albumart);
@@ -79,38 +77,8 @@ public class Song {
     public String getYear() {
         return year;
     }
-    public void setYear(String year) {
-        this.year = year;
-    }
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
     public String getTrack() {
         return track;
-    }
-    public void setTrack(String track) {
-        this.track = track;
-    }
-    public String getComment() {
-        return comment;
-    }
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-    public String getLyrics() {
-        return lyrics;
-    }
-    public void setLyrics(String lyrics) {
-        this.lyrics = lyrics;
-    }
-    public String getFilePath() {
-        return filePath;
-    }
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
     }
     public File getFile() {
         return file;
@@ -118,7 +86,7 @@ public class Song {
     public double getLength() {
         return length;
     }
-    public void setLength(double length) {
-        this.length = length;
+    public Component getComponent() {
+        return new JLabel(getTitle());
     }
 }
--- src/SounderPanel.java
+++ src/SounderPanel.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.util.ArrayList;
 import jaco.mp3.player.MP3Player;
+
+import static javax.swing.BorderFactory.createEmptyBorder;
 
 public class SounderPanel extends JPanel {
     SounderFrame parentFrame;
@@ -21,6 +24,8 @@ public class SounderPanel extends JPanel {
     SongInfo currentSongArtist = new SongInfo("Artist");
     SongInfo currentSongAlbumTitle = new SongInfo("Album");
     SongInfo currentSongDuration = new SongInfo("Duration");
+    SongInfo currentSongTrack = new SongInfo("Track");
+    SongInfo currentSongYear = new SongInfo("Year");
     SounderButton playButton = new SounderButton("▶");
     SounderButton shuffleButton = new SounderButton("\uD83D\uDD01");
     SounderButton repeatButton = new SounderButton("\uD83D\uDD00");
@@ -43,31 +48,41 @@ public class SounderPanel extends JPanel {
         currentSongAlbum = new JLabel(songAlbum);
         currentSongAlbum.setBounds(10, 10, albumSize, albumSize);
         add(currentSongAlbum);
-        songList.setBounds(albumSize + 20, 10, 275, 300);
+        //Song List fit the rest of the UI
         songList.setBackground(foregroundColor);
         songList.setForeground(Color.WHITE);
-        add(songList);
-        addNewSong.setBounds(albumSize + 245, 310, 25, 25);
+        JScrollPane scrollPane = new JScrollPane(songList);
+        //removes annoying border around the list
+        scrollPane.setBorder(createEmptyBorder());
+        scrollPane.setBounds(albumSize + 20, 10, 255, 300);
+        add(scrollPane);
+        addNewSong.setBounds(albumSize + 225, 310, 25, 25);
         add(addNewSong);
-        removeSong.setBounds(albumSize + 270, 310, 25, 25);
+        removeSong.setBounds(albumSize + 250, 310, 25, 25);
         add(removeSong);
+        //all controls for player
         playButton.setBounds(albumSize + 30, 345, 40, 40);
         add(playButton);
         shuffleButton.setBounds(albumSize - 15, 345, 40, 40);
         add(shuffleButton);
         repeatButton.setBounds(albumSize + 75, 345, 40, 40);
         add(repeatButton);
+
+        //controls for volume control
         volumeSlider.setBounds(albumSize + 130, 350, 100, 40);
         volumeSlider.setBackground(backgroundColor);
         volumeSlider.setForeground(foregroundColor);
         volumeSlider.setMajorTickSpacing(10);
         volumeSlider.setPaintTicks(true);
         add(volumeSlider);
+
+        //Makeshift song length slider
         progressSlider.setBounds(50, 400, 400, 40);
         progressSlider.setBackground(backgroundColor);
         progressSlider.setForeground(foregroundColor);
         add(progressSlider);
 
+        //Song info labels
         currentSongTitle.setBounds(10, albumSize + 20, 200, 25);
         add(currentSongTitle);
         currentSongArtist.setBounds(10, albumSize + 45, 200, 25);
@@ -76,83 +91,117 @@ public class SounderPanel extends JPanel {
         add(currentSongAlbumTitle);
         currentSongDuration.setBounds(10, albumSize + 95, 200, 25);
         add(currentSongDuration);
+        currentSongTrack.setBounds(10, albumSize + 120, 200, 25);
+        add(currentSongTrack);
+        currentSongYear.setBounds(10, albumSize + 145, 200, 25);
+        add(currentSongYear);
+
+        //makes the song list look better by padding it
         songList.setCellRenderer(new DefaultListCellRenderer() {
             @Override
             public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
                 JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                label.setBorder(BorderFactory.createEmptyBorder(5, 5, 2, 5));
+                label.setBorder(createEmptyBorder(5, 5, 2, 5));
                 return label;
             }
         });
+
         revalidate();
-        repaint();
     }
+    //adds the songs to the arraylist and UI
     void addSongToList(Song newSong) {
         Songs.add(newSong);
         SongModel.add(SongModel.getSize(), newSong.getTitle());
         songList.setModel(SongModel);
         songList.add(Songs.get(Songs.size() - 1).getComponent());
     }
-    Song addSong() {
+    //adds songs based on what the user chooses
+    Song[] addSong() {
         Song newSong = new Song();
-        newSong.makeSong();
-        return newSong;
+        Song[] songs = newSong.makeSong();
+        return songs;
     }
+    //gets the selected song and removes it from the list and UI
     void removeSong() {
-        Songs.remove(songList.getSelectedIndex());
-        SongModel.remove(songList.getSelectedIndex());
-        songList.setModel(SongModel);
+        if (songSelected()) {
+            Songs.remove(songList.getSelectedIndex());
+            SongModel.remove(songList.getSelectedIndex());
+            songList.setModel(SongModel);
+        }
     }
+    //plays the selected song and calls methods to update the UI
     void playSong() {
         if(isPlaying) {
             player.stop();
             isPlaying = false;
         }
-        if (songList.getSelectedIndex() == -1) {
-            return;
+        if (songSelected()) {
+            File songFile = new File(String.valueOf(Songs.get(songList.getSelectedIndex()).getFile()));
+            player = new MP3Player(songFile);
+            player.play();
+            isPlaying = true;
+            parentFrame.setTitle(Songs.get(songList.getSelectedIndex()).getTitle() + " - Sounder");
+            updateAlbumArt(songList.getSelectedIndex());
+            updateSongInfo(songList.getSelectedIndex());
         }
-        player = new MP3Player(Songs.get(songList.getSelectedIndex()).getFile());
-        player.play();
-        isPlaying = true;
-        parentFrame.setTitle(Songs.get(songList.getSelectedIndex()).getTitle() + " - Sounder");
-        updateAlbumArt(songList.getSelectedIndex());
-        updateSongInfo(songList.getSelectedIndex());
     }
+    //updates the album art of the current song
     void updateAlbumArt(int index) {
         var albumArt = Songs.get(index).getAlbumart();
         songAlbum = albumArt;
         Image img = albumArt.getImage().getScaledInstance(albumSize, albumSize, Image.SCALE_SMOOTH);
         currentSongAlbum.setIcon(new ImageIcon(img));
     }
+    //updates all visual info about the song for UI
     void updateSongInfo(int index) {
         currentSongTitle.setText(Songs.get(index).getTitle());
         currentSongArtist.setText(Songs.get(index).getArtist());
         currentSongAlbumTitle.setText(Songs.get(index).getAlbum());
         currentSongDuration.setText(Songs.get(index).getLength() + "");
+        currentSongTrack.setText("Track #: " + Songs.get(index).getTrack());
+        currentSongYear.setText(Songs.get(index).getYear());
     }
+    //adds event listeners to all buttons
     void addEventListeners() {
-        addNewSong.addActionListener(e -> addSongToList(addSong()));
+        addNewSong.addActionListener(e -> {
+            Song[] songs = addSong();
+            for (Song song : songs) {
+                addSongToList(song);
+            }
+        });
         removeSong.addActionListener(e -> removeSong());
         playButton.addActionListener(e -> toggleSong());
         songList.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON2) {
+                    removeSong();
+                    return;
+                }
                 if (e.getClickCount() == 2) {
                     playSong();
                 }
+
             }
         });
-
     }
+    //for play button toggling
     void toggleSong() {
         if(isPlaying) {
             player.stop();
             isPlaying = false;
             playButton.setText("▶");
         } else {
+            if(player == null) {
+                playSong();
+            }
             player.play();
             isPlaying = true;
             playButton.setText("||");
         }
+    }
+    //checks if a song is selected
+    boolean songSelected() {
+        return songList.getSelectedIndex() != -1;
     }
 }
